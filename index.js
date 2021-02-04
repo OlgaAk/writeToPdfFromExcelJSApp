6c8e38ea3e3eb481d6a8ebe516ac32a0786ba1be
olgaaktas <binpolyglotte@gmail.com>
2021-02-04 09:24:48 +0300

    different type of excel acceptable

--- index.js
+++ index.js
@@ -2,20 +2,34 @@ const { PDFDocument, StandardFonts, rgb } = require("pdf-lib");
 const fs = require("fs");
 const ExcelJS = require("exceljs");
 
-const SOURCE_PDF_PATH = "./1.pdf"; //get path from user
-const OUTPUT_PDF_PATH = "./test.pdf"; //get path from user
-const SOURCE_EXCEL_PATH = "2.xlsx"; //get path from user
+let SOURCE_PDF_PATH = "./00.pdf"; //get path from user
+let OUTPUT_PDF_PATH = "./Belege_Januar_2021.pdf"; //get path from user
+let SOURCE_EXCEL_PATH = "00.xlsx"; //get path from user
 const TEXT_COLOR = rgb(0.41, 0.48, 0.74); //make user define
 const TEXT_SIZE = 15; //make user define
 const COLUMN = "D"; // make user define
-const START_ROW = 10; //make user define
+let START_ROW = 10; //make user define
+let SPACE_FROM_TOP = 25;
+
+let credicard = true;
+if (credicard) {
+  OUTPUT_PDF_PATH = "Belege_Kreditkarte_Januar_2021.pdf";
+  SOURCE_PDF_PATH = "./111.pdf"; //get path from user
+  SOURCE_EXCEL_PATH = "111.xlsx"; //get path from user
+  START_ROW = 7;
+  SPACE_FROM_TOP = 65;
+}
 
 addExcelDataToPdf().catch((err) => console.log(err));
 
 async function addExcelDataToPdf() {
   readExcel()
     .then((data) => {
-      writeToPdf(data);
+      if (data) {
+        writeToPdf(data);
+      } else {
+        console.log("no data from excel");
+      }
     })
     .catch((err) => console.log(err));
 }
@@ -33,20 +47,36 @@ async function writeToPdf(excelData) {
     });
     fs.writeFileSync(OUTPUT_PDF_PATH, await doc.save());
   } catch (err) {
-    console.log(err);
+    if (err.code == "ENOENT") {
+      console.log("Error opening PDF file. Please try another file.");
+    }
   }
 }
 
 async function readExcel() {
   const sheet = await loadExcelSheet();
-  return getCellValues(sheet, START_ROW, COLUMN);
+  try {
+    const cellValues = getCellValues(sheet, START_ROW, COLUMN);
+    return cellValues;
+  } catch (err) {}
 }
 
 function drawTextOnPdf(page, index, data, font, size, color) {
+  console.log(data[index].length > 55, data[index].length, index == 39, index);
+  let mySize = size;
+  if (data[index].length > 70) {
+    mySize = size - 4;
+    console.log(mySize, index);
+  }
+  let myY = SPACE_FROM_TOP;
+  if (index == 38) {
+    myY = 15;
+    console.log(myY);
+  }
   page.drawText(`B#${index + 1} ${data[index]}`, {
     x: 15,
-    y: page.getHeight() - 25,
-    size: size,
+    y: page.getHeight() - myY,
+    size: mySize,
     font: font,
     color: color,
   });
@@ -67,15 +97,23 @@ function checkInputData(excelData, pages) {
 
 async function loadExcelSheet() {
   const workbook = new ExcelJS.Workbook();
-  await workbook.xlsx.readFile(SOURCE_EXCEL_PATH);
-  return (sheet = workbook.worksheets[0]); // make user define
+  try {
+    await workbook.xlsx.readFile(SOURCE_EXCEL_PATH);
+    return (sheet = workbook.worksheets[0]); // make user define
+  } catch (err) {
+    if (err.message.includes("File not found")) {
+      console.log("Error opening EXCEL file. Please try another file.");
+    }
+  }
 }
 
 function getCellValues(sheet, startRow, column) {
   let excelValues = [];
+  console.log("getCellValues");
   while (true) {
     let cell = sheet.getCell(`${column}${startRow}`);
-    if (cell.value == "" || cell.value == "Total:") break; // make user define
+    console.log(cell.value);
+    if (cell.value == "" || cell.value == "Total:" || cell.value == null) break; // make user define
     excelValues.push(cell.value);
     startRow++;
   }
