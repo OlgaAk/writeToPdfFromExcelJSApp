14d15d3e78f41bd724322ea28c1647847a32a167
olgaaktas <binpolyglotte@gmail.com>
2021-02-22 18:45:53 +0300

    added comments

--- parse.js
+++ parse.js
@@ -1,32 +1,37 @@
 const fs = require("fs");
-const tesseract = require("tesseract.js");
+const tesseract = require("tesseract.js"); // OCR package
 
+// stores parsed data before writing to json file
 let jsonData = {
   invoiceData: [],
 };
 
-const imageFilePath = "out.jpg";
-
+const imageFilePath = "out.jpg"; // should be made dynamic
 const pathToOutputJson = "invoce_data.json";
 
+// Mock Data to substitute Tesseract job
 const testText =
   '0401067\nБАНКОВСКИЙ ОРДЕР № 19 29.01.2021\nДата\nСумма Тридцать рублей 00 копеек Вид оп. 17\nпрописью\n|\nНРВ.Глобал Бизнес ГмбхХ, Санкт- 40807810000002500221 30-00\nПетербургское Представительство компании\nКомиссии за исполнение платежей юр.лиц в | 70601810400002740200\nрублях\nНазначение платежа\n{МО80050}Комиссия за исполнение п/п № 19 от 29.01.21 Отметки банка\nАО "КОММЕРЦБАНК (ЕВРАЗИЯ)"\nИсполнено:29.01.2021\nБИК 044525105,\nк/с 30101810300000000105 в ГУ Банка\nРоссии по ЦФО\nНаз. пл.: ПОДПИСИ\n';
 
-extractTextFromPdf2Json();
+/// END Globals
 
-async function extractTextFromPdf2Json() {
-  await extractTextFromAllPdfPages();
+//runs code
+extractTextFromImage2Json();
+
+// Gets text data from each Pdf page, stores it in the global variable and finally writes it to a json file
+async function extractTextFromImage2Json() {
+  await extractTextFromAllImages();
   writeDataToJson(jsonData);
 }
 
-async function extractTextFromAllPdfPages() {
+async function extractTextFromAllImages() {
   for (let i = 0; i < 2; i++) {
-    //extractTextFromPdfPage(imageFilePath, i);
     updateJsonData(testText, i);
+    //extractTextFromImage(filePath, i)
   }
 }
 
-function extractTextFromPdfPage(filePath, id) {
+function extractTextFromImage(filePath, id) {
   tesseract
     .recognize(filePath, "rus", {
       logger: (data) => console.log(data.progress),
@@ -49,10 +54,12 @@ function updateJsonData(text, id) {
 }
 
 function writeDataToJson(jsonData) {
+  //TODO check if exits
   const json = JSON.stringify(jsonData);
   fs.writeFileSync(pathToOutputJson, json, "utf-8");
 }
 
+// Gets needed substrings from text
 function getInvoiceData(text, id) {
   const invoiceDate = getInvoiceDateFromString(text);
   const invoiceSum = getInvoiceSumFromString(text);
@@ -73,6 +80,7 @@ function getInvoiceDateFromString(text) {
 
 function getInvoiceSumFromString(text) {
   if (
+    // sum is placed between two following substrings
     text.indexOf("40807810000002500221") > -1 &&
     text.indexOf("Петербургское Представительство компании") > -1
   ) {
@@ -86,10 +94,11 @@ function getInvoiceSumFromString(text) {
 }
 
 function convertInvoiceSumToInt(invoiceSum) {
-  return Number(invoiceSum.split("-").join("."));
+  return Number(invoiceSum.split("-").join(".")); // input format is dd-dd
 }
 
 function isCorrectDocType(text) {
+  // only doc with this substring are parsed
   return text.indexOf("БАНКОВСКИЙ ОРДЕР") > -1;
 }
 
--- parse.py
+++ parse.py
@@ -1,5 +1,8 @@
 from pdf2image import convert_from_path
 
-pages = convert_from_path("report1.pdf", 500)
-for page in pages:
-    page.save("out.jpg", "JPEG")
+IMAGE_RESOLUTION = 500  # high resolution
+
+# Converts pdf file to a jpeg image
+pages = convert_from_path("report1.pdf", IMAGE_RESOLUTION)
+for i, page in enumerate(pages):
+    page.save(f"out{i}.jpg", "JPEG")
